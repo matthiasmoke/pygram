db6975e95dc57fd4cde71666a9cdba3179f2c245
matthiasmoke <matthias.rauch.98@outlook.com>
2022-03-06 12:13:55 +0100

    rework variable type cache scope handling

--- src/type_retrieval/variable_type_cache.py
+++ src/type_retrieval/variable_type_cache.py
@@ -1,6 +1,8 @@
 import logging
 from enum import Enum
-from typing import Dict
+from typing import Dict, List
+
+from .preprocessed_type_caches import TypeCache
 from .type_info import TypeInfo
 
 logger = logging.getLogger("main")
@@ -12,61 +14,67 @@ class Scope(Enum):
 
 class VariableTypeCache:
 
-    def __init__(self, module_path: str) -> None:
-        self.module_path: str = module_path # TODO get fully qualified module name
-        self.class_scope_name = ""
+    def __init__(self, module_path: str, project_type_cache: TypeCache) -> None:
+        self.module_path: str = module_path
+        self._project_type_cache: TypeCache = project_type_cache
         self.function_scope_name = ""
 
+        self.scope_stack: List[Scope] = []
+        self.class_scope_stack: List[str] = []
+
         self.module_variables: Dict[str, TypeInfo] = {}
-        self.class_variables: Dict[str, TypeInfo] = {}
+        self.class_scopes: Dict[str, Dict[str, TypeInfo]] = {}
         self.function_variables: Dict[str, TypeInfo] = {}
-        self.previous_scope = Scope.MODULE
-        self.current_scope: Scope = Scope.MODULE
     
     def set_class_scope(self, name: str) -> None:
-        self.class_scope_name = name
-        self.previous_scope = self.current_scope
-        self.current_scope = Scope.CLASS
+        if len(self.class_scopes):
+            name = self._get_inner_class_path(name)
+
+        self.scope_stack.append(Scope.CLASS)
+        self.class_scope_stack.append(name)
+        
+        # create cache for class and add self type
+        class_type = TypeInfo(label=name)
+        self._project_type_cache.populate_type_info_with_module(class_type)
+        self.class_scopes[name] = {}
+        self.class_scopes[name]["self"] = class_type
 
     def leave_class_scope(self):
-        self.class_scope_name = ""
-        self.previous_scope = Scope.CLASS
-        self.current_scope = Scope.MODULE
-        self.class_variables.clear()
+        left_class: str = self.class_scope_stack.pop()
+        del self.class_scopes[left_class]
+        self.scope_stack.pop()
     
     def set_function_scope(self, name: str):
         self.function_scope_name = name
-        self.previous_scope = self.current_scope
-        self.current_scope = Scope.FUNCTION
+        self.scope_stack.append(Scope.FUNCTION)
 
     def leave_function_scope(self):
         self.function_scope_name = ""
         self.function_variables.clear()
-        self.current_scope = self.previous_scope
-        self.previous_scope = Scope.FUNCTION
+        self.scope_stack.pop()
 
     def add_variable(self, variable_name: str, variable_type: TypeInfo):
-        scope: Scope = self.current_scope
+        scope: Scope = self._get_current_scope()
         if scope == Scope.MODULE:
             self.module_variables[variable_name] = variable_type
         elif scope == Scope.CLASS:
-            self.class_variables[variable_name] = variable_type
+            self._set_class_variable(variable_name, variable_type)
         else:
-            if self.function_scope_name == "__init__" and self.previous_scope == Scope.CLASS:
-                self.class_variables[variable_name] = variable_type
+            if self.function_scope_name == "__init__" and self._get_previous_scope() == Scope.CLASS:
+                self._set_class_variable(variable_name, variable_type)
             else:
                 self.function_variables[variable_name] = variable_type
 
 
     def get_variable_type(self, variable_name: str, depth: int, subscript_index: int) -> TypeInfo:
-        scope: Scope = self.current_scope
-        previous_scope: Scope = self.previous_scope
+        scope: Scope = self._get_current_scope()
+        previous_scope: Scope = self._get_previous_scope()
         variable_type: TypeInfo = None
         if scope == Scope.FUNCTION:
             variable_type = self.function_variables.get(variable_name, None)
         
         if previous_scope == Scope.CLASS and variable_type is None:
-            variable_type = self.class_variables.get(variable_name, None)
+            variable_type = self._get_class_variable(variable_name)
         
         if variable_type is None:
             variable_type = self.module_variables.get(variable_name, None)
@@ -85,3 +93,28 @@ class VariableTypeCache:
 
         return variable_type
     
+    def _get_inner_class_path(self, name: str) -> str:
+        output: str = ""
+        if name is not None:
+            for key in self.class_scopes:
+                output += "{}.".format(key)
+            output += name
+        return output
+    
+    def _get_current_scope(self) -> Scope:
+        if len(self.scope_stack) == 0:
+            return Scope.MODULE
+        return self.scope_stack[-1]
+    
+    def _get_previous_scope(self) -> Scope:
+        if len(self.scope_stack) < 2:
+            return Scope.MODULE
+        return self.scope_stack[-2]
+    
+    def _set_class_variable(self, variable_name: str, type_info: TypeInfo) -> None:
+        current_class = self.class_scope_stack[-1]
+        self.class_scopes[current_class][variable_name] = type_info
+    
+    def _get_class_variable(self, variable_name: str) -> TypeInfo:
+        current_class = self.class_scope_stack[-1]
+        return self.class_scopes[current_class].get(variable_name, None)
